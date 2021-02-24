42bde397cac6036c282b070c109713dd0c34b374
maximumadmin <61138453+maximumadmin@users.noreply.github.com>
2021-02-24 11:13:47 +0900

    Proper env variable names

--- src/zramd.go
+++ src/zramd.go
@@ -16,9 +16,9 @@ import (
 // https://fedoraproject.org/wiki/Changes/Scale_ZRAM_to_full_memory_size.
 type startCmd struct {
 	Algorithm    string  `arg:"-a,env" default:"zstd" placeholder:"A" help:"zram compression algorithm"`
-	MaxSizeMB    int     `arg:"-m,--max-size,env" default:"8192" placeholder:"M" help:"maximum total MB of swap to allocate"`
+	MaxSizeMB    int     `arg:"-m,--max-size,env:MAX_SIZE" default:"8192" placeholder:"M" help:"maximum total MB of swap to allocate"`
 	Fraction     float32 `arg:"-f,env" default:"1.0" placeholder:"F" help:"maximum percentage of RAM allowed to use"`
-	SwapPriority int     `arg:"-p,--priority,env" default:"10" placeholder:"N" help:"swap priority"`
+	SwapPriority int     `arg:"-p,--priority,env:PRIORITY" default:"10" placeholder:"N" help:"swap priority"`
 }
 
 type stopCmd struct {
