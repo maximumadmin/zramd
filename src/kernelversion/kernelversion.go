ad236834d7ebbed6e6076b07d7b8db82ee6dd57d
maximumadmin <61138453+maximumadmin@users.noreply.github.com>
2021-02-28 13:21:40 +0900

    More tests

--- src/kernelversion/kernelversion.go
+++ src/kernelversion/kernelversion.go
@@ -4,14 +4,23 @@ import "zramd/src/utsname"
 
 var major, minor = utsname.Uname().KernelVersion()
 
+type kVersion struct {
+	major int
+	minor int
+}
+
+func gte(a kVersion, b kVersion) bool {
+	return (a.major == b.major && a.minor >= b.minor) || a.major > b.major
+}
+
 // SupportsZram checks if current kernel version supports zram.
 func SupportsZram() bool {
-	return (major == 3 && minor >= 14) || major > 3
+	return gte(kVersion{major, minor}, kVersion{3, 14})
 }
 
 // SupportsZstd checks if current kernel supports zstd compressed zram.
 func SupportsZstd() bool {
-	return (major == 4 && minor >= 19) || major > 4
+	return gte(kVersion{major, minor}, kVersion{4, 19})
 }
 
 // SupportsMultiCompStreams checks if current kernel supports multiple
@@ -19,5 +28,5 @@ func SupportsZstd() bool {
 // multiple processors with a single zram device, see also
 // https://wiki.gentoo.org/wiki/Zram#Caveats.2FCons.
 func SupportsMultiCompStreams() bool {
-	return (major == 3 && minor >= 15) || major > 3
+	return gte(kVersion{major, minor}, kVersion{3, 15})
 }
